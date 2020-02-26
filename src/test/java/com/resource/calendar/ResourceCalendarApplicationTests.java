d8b3a72d3b9229c1ee4d4ffb5065af40e7813a81
Uttam Patil <uttam.patil@parkar.local>
2020-02-26 13:23:19 +0530

    project skeleton

---
+++ src/test/java/com/resource/calendar/ResourceCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resource.calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
